25324fb189f07963839c93ca08703bf9b621968f
Hussain1402 <141639385+Hussain1402@users.noreply.github.com>
2024-07-30 15:32:31 +0530

    Create Even_Odd.cpp

---
+++ Even_Odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enetr the number to be checked:"<<endl;
+    int a;
+    cin>>a;
+    if(a%2==0){
+        cout<<"Even number.";
+    }
+    else{
+        cout<<"Odd number";
+    }
+    return 0;
+}
